59e25760e9fa056850ff63654d56094f1644f2a4
Alex Zhidkov <sashazudkov@gmail.com>
2022-06-23 17:29:36 +0300

    add auto-approve and tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -178,6 +178,68 @@ def test_random_choice_when_heads_and_tails(choice_mock, n, expected):
         ),
     ],
 )
+@mock.patch('random.choice')
+def test_random_choice_when_heads_and_tails(choice_mock, n, expected):
+    code = inspect.getsource(flip_coin)
+    parsed_code = ast.parse(code)
+
+    if "choice" in ast.dump(parsed_code):
+        test_case = []
+
+        cases = generate_cases(n)
+
+        for counter in range(10000):
+            for i in range(10):
+                if i + counter in cases[i]:
+                    test_case += ["tail"]
+                else:
+                    test_case += ["eagle"]
+
+        choice_mock.side_effect = test_case
+
+        try:
+            assert flip_coin() == expected
+        except KeyError:
+            return
+
+
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        pytest.param(
+            10,
+            {
+                0: 0.01,
+                1: 0.0,
+                2: 0.28,
+                3: 3.85,
+                4: 25.97,
+                5: 69.8,
+                6: 0.04,
+                7: 0.01,
+                8: 0.03,
+                9: 0.01,
+                10: 0.0
+            }
+        ),
+        pytest.param(
+            7,
+            {
+                0: 0.06,
+                1: 0.78,
+                2: 3.45,
+                3: 7.59,
+                4: 15.24,
+                5: 72.79,
+                6: 0.03,
+                7: 0.02,
+                8: 0.02,
+                9: 0.02,
+                10: 0.0
+            }
+        ),
+    ],
+)
 @mock.patch('random.randint')
 def test_random_choice_when_zero_and_one(choice_mock, n, expected):
     code = inspect.getsource(flip_coin)
